8bec852dcc5abeaa8be10bc569ccc696598650c1
Peter Nicholls <peterwnicholls@gmail.com>
2014-08-18 17:31:53 +1000

    added a ldap_database_authenticatable_fallback option

--- lib/devise_ldap_authenticatable/strategy.rb
+++ lib/devise_ldap_authenticatable/strategy.rb
@@ -5,15 +5,24 @@ module Devise
     class LdapAuthenticatable < Authenticatable
       def authenticate!
         resource = mapping.to.find_for_ldap_authentication(authentication_hash.merge(password: password))
-        
+
         if resource && validate(resource) { resource.valid_ldap_authentication?(password) }
           resource.after_ldap_authentication
           success!(resource)
+        elsif ::Devise.ldap_database_authenticatable_fallback
+          resource  = valid_password? && mapping.to.find_for_database_authentication(authentication_hash)
+          encrypted = false
+
+          if validate(resource){ encrypted = true; resource.valid_password?(password) }
+            resource.after_database_authentication
+            success!(resource)
+          end
+
+          mapping.to.new.password = password if !encrypted && Devise.paranoid
+          fail(:not_found_in_database) unless resource
         else
           return fail(:invalid)
         end
-
-
       end
     end
   end
